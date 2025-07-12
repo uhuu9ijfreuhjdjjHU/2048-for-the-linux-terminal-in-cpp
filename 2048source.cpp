f4af1b93a39a2a7292eb401728fd9b09e4e5e3f2
ami3 <riverknuuttila2@outlook.com>
2025-07-11 23:15:54 -0400

    Added full project.
    Enjoy.

---
+++ 2048source.cpp
@@ -0,0 +1,154 @@
+
+  //2048 clone in the linux terminal or windows terminal.
+  //Copyright (C) 2025 River Knuuttila, common alias, Annie.
+
+  //This program is free software: you can redistribute it and/or modify
+  //it under the terms of the GNU General Public License as published by
+  //the Free Software Foundation, either version 3 of the License, or
+  //(at your option) any later version.
+
+  //This program is distributed in the hope that it will be useful,
+  //but WITHOUT ANY WARRANTY; without even the implied warranty of
+  //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  //GNU General Public License for more details.
+
+  //You should have received a copy of the GNU General Public License
+  //along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  
+  //Concerns or questions? Reach out at: riverknuuttila2@outlook.com
+
+#include <sstream>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <list>
+#include <stdio.h>
+#include <wchar.h>
+#include <string>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+#include "/home/ami3/codejunks/finishedProjects/2048/projectfiles/projectLibrary.hpp" //change depending on your file path.
+
+int main(){
+  
+  ifstream file("/home/ami3/codejunks/finishedProjects/2048/projectfiles/copywriteNotice.txt"); //change depending on your file path.
+    
+  if (!file) 
+  {
+    cerr<<"Could not open the file!"<<endl;
+    return 1;
+  }
+
+  stringstream uffer;
+  uffer<<file.rdbuf();
+
+  string contentS=uffer.str();
+
+  cout<<contentS<<endl;
+
+  string noticeA;
+
+  cin>>noticeA;
+
+if (noticeA=="r")
+{
+  int playingGrid[4][4] = {0}; //multidimensional array initialized to zero. represented by logic of functions above.
+
+  newRandomBox(playingGrid);
+    newRandomBox(playingGrid);
+
+  clearScreen();
+    printGame(playingGrid);
+
+  setBufferedInput(false);
+
+  while (true) //main gamestate.
+  {
+    char cont = getchar();
+
+    if (cont == 'q')
+      break;
+
+    bool moved = false;
+
+    if (cont == '\033')
+    {
+      if (getchar() == '[')
+      {
+        char direction = getchar();
+        switch (direction) //switch statement for player inputs
+        {
+        case 'A': // up key
+          moved = moveUp(playingGrid);
+            break;
+        case 'B': // down key
+            moved = moveDown(playingGrid);
+            break;
+        case 'C': // right key
+          moved = moveRight(playingGrid);
+            break;
+        case 'D': // left key
+          moved = moveLeft(playingGrid);
+            break;
+        }
+      }
+    }
+
+    if (moved)
+    {
+      newRandomBox(playingGrid);
+        clearScreen();
+          printGame(playingGrid);
+
+      if (!canMove(playingGrid)) //lose condition.
+      {
+        cout<<"Game Over!" << endl;
+        break;
+      }
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+      for (int j = 0; j<4; j++)
+      {
+        if (playingGrid[i][j]==2048)
+        {
+          cout<<" GG, you win! :3"<<endl; //win condition.
+          break;
+        }
+      }
+    }
+  }
+
+  setBufferedInput(true);
+}
+else if (noticeA=="d")
+{
+  
+  ifstream file("/home/ami3/codejunks/finishedProjects/2048/projectfiles/License text"); //change depending on your file path.
+    
+  if (!file) 
+  {
+    cerr<<"Could not open the file!"<<endl;
+    return 1;
+  }
+
+  stringstream uuffer;
+  uuffer<<file.rdbuf();
+
+  string ccontentS=uuffer.str();
+
+  cout<<ccontentS<<endl;
+}
+else if (noticeA=="q")
+{
+  cout<<"Exiting!";
+}
+return 0;}
---
+++ projectfiles/projectLibrary.hpp
@@ -0,0 +1,322 @@
+#include <sstream>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <list>
+#include <stdio.h>
+#include <wchar.h>
+#include <string>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+void clearScreen() //self explanetory, clears screen.
+{
+  #ifdef _WIN32
+    system("cls");
+  #else
+    std::cout<<"\033[2J\033[H";
+  #endif
+}
+
+void setBufferedInput(bool enable) //sets terminal input to repeat and detect without `enter`.
+{
+  static bool enabled = true;
+  static struct termios old;
+  struct termios newt;
+
+  if (enable && !enabled)
+  {
+    tcsetattr(STDIN_FILENO, TCSANOW, &old);
+    enabled = true;
+  }
+  else if (!enable && enabled)
+  {
+    tcgetattr(STDIN_FILENO, &old);
+    newt = old;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    enabled = false;
+  }
+}
+
+void emptyBox() //prints empty box.
+{
+  cout<<"|    ";
+}
+
+void printBox(int displayNumber) // prints box depending on the amoung of digits the number has.
+{
+  if (displayNumber >= 1000)
+  {
+    cout<<"|"<<displayNumber;
+  }
+  
+  else if (displayNumber >= 100)
+  {
+    cout<<"|"<<displayNumber<<" ";
+  }
+  
+  else if (displayNumber >= 10)
+  {
+    cout<<"|"<<displayNumber<<"  ";
+  }
+  
+  else if (displayNumber > 0)
+  {
+    cout<<"| "<<displayNumber<<"  ";
+  }
+  
+  else
+  {
+    emptyBox();
+  }
+}
+
+int getRandomNumber(int min, int max) //also pretty self explanetory, generates a random number based on a minimum and maximum.
+{
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<> distr(min, max);
+  return distr(gen);
+}
+
+void printGame(int playingGrid[4][4]) //prints the playingGrid and cubes containing numbers.
+{
+  for (int i = 0; i < 4; i++)
+  {
+    cout<<"_____________________" << endl;
+    for (int j = 0; j < 4; j++)
+    {
+      printBox(playingGrid[i][j]);
+    }
+    cout<<"|"<<endl;
+  }
+  cout<<"_____________________" << endl;
+}
+
+bool newRandomBox(int playingGrid[4][4]) //prints new random box with '2' everytime the player makes a valid move
+{
+  vector<pair<int, int>> emptyCells;
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      if (playingGrid[i][j] == 0)
+      {
+        emptyCells.push_back({i, j});
+      }
+    }
+  }
+  
+  if (emptyCells.empty())
+  {
+    return false;
+  }
+  
+  auto [row, col] = emptyCells[getRandomNumber(0, emptyCells.size() - 1)];
+  playingGrid[row][col] = (getRandomNumber(0, 9) < 9) ? 2 : 4;
+  return true;
+}
+
+//lines 130-281 contains the functions for moving values through the array.
+
+bool moveLeft(int grid[4][4])
+{
+  bool moved = false;
+  for (int i = 0; i < 4; i++)
+  {
+    vector<int> row;
+    for (int j = 0; j < 4; j++)
+    {
+      if (grid[i][j] != 0)
+      {
+        row.push_back(grid[i][j]);
+      }
+    }
+
+    for (int j = 0; j < (int)row.size() - 1; j++)
+    {
+      if (row[j] == row[j + 1])
+      {
+        row[j] *= 2;
+        row.erase(row.begin() + j + 1);
+        moved = true;
+      }
+    }
+
+    while (row.size() < 4)
+    {
+      row.push_back(0);
+    }
+    
+    for (int j = 0; j < 4; j++)
+    {
+      if (grid[i][j] != row[j])
+      {
+        grid[i][j] = row[j];
+        moved = true;
+      }
+    }
+  }
+  return moved;
+}
+
+bool moveRight(int grid[4][4])
+{
+  bool moved = false;
+  for (int i = 0; i < 4; i++)
+  {
+    vector<int> row;
+    for (int j = 3; j >= 0; j--)
+    {
+      if (grid[i][j] != 0)
+        row.push_back(grid[i][j]);
+    }
+
+    for (int j = 0; j < (int)row.size() - 1; j++)
+    {
+      if (row[j] == row[j + 1])
+      {
+        row[j] *= 2;
+        row.erase(row.begin() + j + 1);
+        moved = true;
+      }
+    }
+
+    while (row.size() < 4)
+      row.push_back(0);
+
+    for (int j = 0; j < 4; j++)
+    {
+      if (grid[i][3 - j] != row[j])
+      {
+        grid[i][3 - j] = row[j];
+        moved = true;
+      }
+    }
+  }
+  return moved;
+}
+
+bool moveUp(int grid[4][4])
+{
+  bool moved = false;
+  for (int j = 0; j < 4; j++)
+  {
+    vector<int> col;
+    for (int i = 0; i < 4; i++)
+    {
+      if (grid[i][j] != 0)
+      {
+      col.push_back(grid[i][j]);
+      }
+    }
+
+    for (int i = 0; i < (int)col.size() - 1; i++)
+    {
+      if (col[i] == col[i + 1])
+      {
+        col[i] *= 2;
+        col.erase(col.begin() + i + 1);
+        moved = true;
+      }
+    }
+
+    while (col.size() < 4)
+      col.push_back(0);
+
+    for (int i = 0; i < 4; i++)
+    {
+      if (grid[i][j] != col[i])
+      {
+        grid[i][j] = col[i];
+        moved = true;
+      }
+    }
+  }
+  return moved;
+}
+
+bool moveDown(int grid[4][4])
+{
+  bool moved = false;
+  for (int j = 0; j < 4; j++)
+  {
+    vector<int> col;
+    for (int i = 3; i >= 0; i--)
+    {
+      if (grid[i][j] != 0)
+        col.push_back(grid[i][j]);
+    }
+    for (int i = 0; i < (int)col.size() - 1; i++)
+    {
+      if (col[i] == col[i + 1])
+      {
+        col[i] *= 2;
+        col.erase(col.begin() + i + 1);
+        moved = true;
+      }
+    }
+
+    while (col.size() < 4)
+      col.push_back(0);
+
+    for (int i = 0; i < 4; i++)
+    {
+      if (grid[3 - i][j] != col[i])
+      {
+        grid[3 - i][j] = col[i];
+        moved = true;
+      }
+    }
+  }
+  return moved;
+}
+
+bool canMove(int grid[4][4]) //can it move?
+{
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      if (grid[i][j] == 0)
+      {  
+        return true;
+      } 
+    } 
+  }
+
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {  
+      if (grid[i][j] == grid[i][j + 1])
+      {
+        return true;
+      }
+    }
+  }
+
+  for (int j = 0; j < 4; j++)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      if (grid[i][j] == grid[i + 1][j])
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
